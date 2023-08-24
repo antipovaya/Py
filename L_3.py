6b3103c02d58d69024fe612e541c572305c2b7cf
Yana Antipova <antipovayana01@gmail.com>
2023-08-24 10:45:25 +0300

    Семинар_3

--- L_3.py
+++ L_3.py
@@ -10,7 +10,7 @@
 # n = random.randint(1, 100)
 # print(n)
 
-from random import randint
+# from random import randint
 
 # n = int(input())
 # list_1 = [randint(1, 10) for i in range(n)]
@@ -28,9 +28,10 @@ from random import randint
 #
 #     if list_1[i - 1] != list_1[i]:
 #         count += 1
-#
+# 2 3 3 4 5 3 6  2 3 3 3 4 5 6
 # print(count)
 
+#######################################################################################################################
 
 # Задача №19. Решение в группах
 # Дана последовательность из N целых чисел и число
@@ -40,7 +41,72 @@ from random import randint
 # Input: [1, 2, 3, 4, 5] k = 3
 # Output: [4, 5, 1, 2, 3]
 
-n, k = int(input()), int(input())
+# n, k = int(input()), int(input())
+#
+# list_1 = [i + 1 for i in range(n)]
+# print(list_1)
+# list_2 = [i + 1 for i in range(n)]
+#
+# for i in range(k):
+#
+#     list_2[len(list_2) - k + i] = list_1[i]
+# print(list_2)
+#
+# for i in range(n-k):
+#     list_2[i] = list_1[i + k]
+# print(list_2)
 
-list_1 = [i + 1 for i in range(n)]
+
+# Решение через срезы
+
+# n, k = int(input()), int(input())
+#
+# list_1 = [i + 1 for i in range(n)]
+# print(list_1)
+#
+# print(list_1[:k])
+#
+# print(list_1[k:len(list_1)])
+#
+# print(list_1[k:len(list_1)] + list_1[:k])
+
+
+#######################################################################################################################
+
+# Задача №21. Решение в группах
+# Напишите программу для печати всех уникальных
+# значений в словаре.
+# Input: [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"},
+# {"VI": "S005"}, {"VII": " S005 "}, {" V ":" S009 "}, {" VIII
+# ":" S007 "}]
+# Output: {'S005', 'S002', 'S007', 'S001', 'S009'}
+
+
+# d = {'up': '↑', 'left': '←', 'down': '↓', 'right': '→'}
+# for(k, v) in d.items():
+#     print(v)
+
+
+#######################################################################################################################
+
+# Задача №23. Решение в группах
+# Дан массив, состоящий из целых чисел. Напишите
+# программу, которая подсчитает количество
+# элементов массива, больших предыдущего (элемента
+# с предыдущим номером)
+# Input: [0, -1, 5, 2, 3]
+# Output: 2 (-1 < 5, 2 < 3)
+
+
+from random import randint
+
+n = int(input())
+list_1 = [randint(1, 10) for i in range(n)]
 print(list_1)
+count = 0
+for i in range(1, len(list_1)):
+
+    if list_1[i - 1] < list_1[i]:
+        count += 1
+
+print(count)
