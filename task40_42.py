f429ccdd3aa0d93348e56ba8699cd2d129e18b09
Yana Antipova <antipovayana01@gmail.com>
2023-09-18 15:07:49 +0300

    ДЗ 9

---
+++ task40_42.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+df = pd.read_csv('california_housing_train.csv')
+
+# Задача 40: Работать с файлом california_housing_train.csv, который находится в папке sample_data.
+# Определить среднюю стоимость дома, где кол-во людей от 0 до 500 (population).
+
+print(df[(df['population'] >= 0) & (df['population'] <= 500)]['median_house_value'])
+print(df[(df['population'] >= 0) & (df['population'] <= 500)]['median_house_value'].mean())
+
+# Задача 42: Узнать какая максимальная households в зоне минимального значения population.
+
+print(df[df['population'] == df['population'].min()]['households'].max())
